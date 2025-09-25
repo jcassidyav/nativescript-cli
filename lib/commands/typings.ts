8e965ef99dbec019982ea6421388557825dbe0d3
Jason Cassidy <jcassidy@avaeon.com>
2025-09-25 09:55:16 +0100

    feat: typings from source iniial

--- lib/commands/typings.ts
+++ lib/commands/typings.ts
@@ -7,7 +7,16 @@ import { IChildProcess, IFileSystem, IHostInfo } from "../common/declarations";
 import { ICommand, ICommandParameter } from "../common/definitions/commands";
 import { injector } from "../common/yok";
 import { IOptions, IStaticConfig } from "../declarations";
-import { IProjectData } from "../definitions/project";
+import {
+	IProjectConfigService,
+	IProjectData,
+	IProjectDataService,
+} from "../definitions/project";
+import * as fastGlob from "fast-glob";
+import {
+	androidAppResourcesFolderName,
+	iOSAppResourcesFolderName,
+} from "../constants";
 
 export class TypingsCommand implements ICommand {
 	public allowedParameters: ICommandParameter[] = [];
@@ -21,6 +30,8 @@ export class TypingsCommand implements ICommand {
 		private $hostInfo: IHostInfo,
 		private $staticConfig: IStaticConfig,
 		private $prompter: IPrompter,
+		private $projectDataService: IProjectDataService,
+		private $projectConfigService: IProjectConfigService,
 	) {}
 
 	public async execute(args: string[]): Promise<void> {
@@ -30,9 +41,14 @@ export class TypingsCommand implements ICommand {
 			result = await this.handleAndroidTypings();
 		} else if (this.$mobileHelper.isiOSPlatform(platform)) {
 			result = await this.handleiOSTypings();
+		} else if (platform === "source") {
+			this.$logger.info(
+				"Generating typings from source is not supported yet. Please specify a platform.",
+			);
+			result = await this.handleSourceTypings();
 		}
 		let typingsFolder = "./typings";
-		if (this.$options.copyTo) {
+		if (this.$options.copyTo && platform !== "source") {
 			this.$fs.copyFile(
 				path.resolve(this.$projectData.projectDir, "typings"),
 				this.$options.copyTo,
@@ -50,6 +66,9 @@ export class TypingsCommand implements ICommand {
 
 	public async canExecute(args: string[]): Promise<boolean> {
 		const platform = args[0];
+		if ("source" === platform) {
+			return true;
+		}
 		this.$mobileHelper.validatePlatformName(platform);
 		return true;
 	}
@@ -236,6 +255,181 @@ export class TypingsCommand implements ICommand {
 			},
 		);
 	}
+
+	private async handleSourceTypings(): Promise<boolean> {
+		// collect java/kotlin/swift files
+		let sourceFiles: SourceFiles;
+		// supplied glob ?
+		if (this.$options.filter) {
+			this.$logger.info(
+				`this is the path ${this.$options.filter} ${this.$options.copyTo}`,
+			);
+			const sourceFiles = await this.getRootGroup(this.$options.filter);
+			sourceFiles.other = [];
+		} else {
+			path.posix;
+			// default to resources and ios.NativeSource
+			const appDirectoryPath =
+				this.$projectData.getAppResourcesRelativeDirectoryPath();
+			const iosNativeSource = `${path.posix.join(appDirectoryPath, iOSAppResourcesFolderName, "src")}/**/*`;
+			const androidResources = `${path.posix.join(appDirectoryPath, androidAppResourcesFolderName, "src", "main", "java")}/**/*`;
+			this.$logger.info(
+				`Searching for source files in ${iosNativeSource} and ${androidResources}`,
+			);
+			const sourceFilesIos = await this.getRootGroup(iosNativeSource);
+			const sourceFilesAndroid = await this.getRootGroup(androidResources);
+			sourceFiles = new SourceFiles();
+			sourceFiles.java = sourceFilesAndroid.java;
+			sourceFiles.kotlin = sourceFilesAndroid.kotlin;
+			sourceFiles.swift = sourceFilesIos.swift;
+
+			// handle NativeSource in ns config file
+
+			const nativeSource = this.$projectConfigService.getValue(
+				"ios.NativeSources",
+				[],
+			);
+
+			if (nativeSource?.length) {
+				for (const source of nativeSource) {
+					const sourceGroup = await this.getRootGroup(source.src);
+					sourceFiles.swift.push(...sourceGroup.swift);
+				}
+			}
+		}
+
+		this.$logger.info(`this is the files ${JSON.stringify(sourceFiles)}`);
+		let destinationRoot: string;
+
+		// get where to place generated typings
+
+		if (this.$options.copyTo) {
+			const projectRoot = this.$projectData.projectDir;
+			const fullPath = path.join(this.$options.copyTo, projectRoot);
+			this.$fs.ensureDirectoryExists(fullPath);
+			const stats = this.$fs.getFsStats(fullPath);
+			if (stats.isDirectory()) {
+				destinationRoot = fullPath;
+			}
+		}
+		if (destinationRoot === undefined) {
+			destinationRoot = path.resolve(
+				this.$projectData.getAppDirectoryRelativePath(),
+				"types",
+			);
+		}
+
+		this.$logger.info(`Generating typings in ${destinationRoot}`);
+
+		// default
+
+		await this.processSwiftSources(sourceFiles.swift, destinationRoot);
+		await this.processJavaSources(sourceFiles.java, destinationRoot);
+		await this.processKotlinSources(sourceFiles.kotlin, destinationRoot);
+
+		return false;
+	}
+	private async processSwiftSources(
+		sources: string[],
+		destinationRoot: string,
+	): Promise<void> {
+		if (sources?.length) {
+			if (!this.$hostInfo.isDarwin) {
+				this.$logger.warn(
+					"Swift source processing is only supported on macOS hosts.",
+				);
+				return;
+			}
+
+			for (const source of sources) {
+				await this.$childProcess.exec(
+					`${path.join(__dirname, "../../node_modules/@nativescript/types-auto-src/platforms/Swift2TS/Swift2TS")} ${source} ${destinationRoot}${path.sep}swift`,
+				);
+			}
+		}
+	}
+	private async processJavaSources(
+		sources: string[],
+		destinationRoot: string,
+	): Promise<void> {
+		if (sources?.length > 0) {
+			const appDirectoryPath = this.$projectData.getAppResourcesDirectoryPath();
+			const androidResources = `${path.join(appDirectoryPath, androidAppResourcesFolderName, "src", "main", "java")}`;
+
+			for (const source of sources) {
+				await this.$childProcess.exec(
+					`java -jar ${path.join(__dirname, "../../node_modules/@nativescript/types-auto-src/platforms/Java2TS/java2TS.jar")} ${androidResources} ${source}  ${destinationRoot}${path.sep}java`,
+				);
+			}
+		}
+	}
+	private async processKotlinSources(
+		sources: string[],
+		destinationRoot: string,
+	): Promise<void> {
+		if (sources?.length > 0) {
+			const appDirectoryPath = this.$projectData.getAppResourcesDirectoryPath();
+			const androidResources = `${path.join(appDirectoryPath, androidAppResourcesFolderName, "src", "main", "java")}`;
+
+			for (const source of sources) {
+				await this.$childProcess.exec(
+					`java -jar ${path.join(__dirname, "../../node_modules/@nativescript/types-auto-src/platforms/Kotlin2TS/Kotlin2TS.jar")} ${androidResources} ${source}  ${destinationRoot}${path.sep}java`,
+				);
+			}
+		}
+	}
+
+	private async getRootGroup(rootPath: string): Promise<SourceFiles> {
+		const fileSources: SourceFiles = new SourceFiles();
+		const projectRoot = this.$projectDataService.getProjectData().projectDir;
+
+		if (fastGlob.isDynamicPattern(rootPath)) {
+			const filePaths = await fastGlob(rootPath);
+			for (const filePath of filePaths) {
+				const sourceFilePath = path.normalize(path.join(projectRoot, filePath));
+				fileSources[this.detectLanguage(sourceFilePath)].push(sourceFilePath);
+			}
+		} else {
+			if (this.$fs.exists(rootPath)) {
+				const stats = this.$fs.getFsStats(rootPath);
+				if (stats.isDirectory() && !this.$fs.isEmptyDir(rootPath)) {
+					this.$fs.readDirectory(rootPath).forEach((fileName) => {
+						const sourceFilePath = path.join(projectRoot, fileName);
+						fileSources[this.detectLanguage(sourceFilePath)].push(
+							sourceFilePath,
+						);
+					});
+				}
+			}
+		}
+
+		return fileSources;
+	}
+
+	private detectLanguage(
+		filePath: string,
+	): "java" | "kotlin" | "swift" | "other" {
+		const extension = filePath.split(".").pop()?.toLowerCase();
+
+		switch (extension) {
+			case "java":
+				return "java";
+			case "kt":
+			case "kts":
+				return "kotlin";
+			case "swift":
+				return "swift";
+			default:
+				return "other";
+		}
+	}
+}
+
+class SourceFiles {
+	swift: string[] = [];
+	java: string[] = [];
+	kotlin: string[] = [];
+	other: string[] = [];
 }
 
 injector.registerCommand("typings", TypingsCommand);
